34e7623cd0d59fd3e8a0ca6257b937c8ff81cf5f
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-12-03 19:46:46 +0300

    15

---
+++ Examples015_MassivSdvig/Program.cs
@@ -0,0 +1,51 @@
+﻿void Massiv(int n, int k)
+{
+    int[] array = new int[n];
+    int[] arrayNew = new int[n];
+    for (int i = 0; i < n;i++)
+    {
+        array[i] = new Random().Next(10);
+        Console.Write($"{array[i]} ");   
+    }
+
+    Console.WriteLine();
+    int j = 0;
+
+    if (k > 0)
+    {   
+        for (int i = 0; i<n; i++)
+        {
+            if (i+k <= n-1)
+            
+                arrayNew[i+k] = array[i];
+                
+            else
+            {
+                arrayNew[j] = array[i];
+                j++;
+            }
+        }   
+    }       
+    else
+        for (int i = 0; i<n; i++)
+        {
+            if ( i - k <= n-1)
+            
+                arrayNew[i] = array[i-k];
+            
+            else
+            {
+                arrayNew[i] = array[j];
+                j++;
+            }
+        } 
+
+    for (int i = 0; i < n; i++)
+        Console.Write($"{arrayNew[i]} ");          
+}
+
+Console.WriteLine("Введите число элементов массива:");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число, на которое делать сдвиг:");
+int K = int.Parse(Console.ReadLine());
+Massiv(N, K);
